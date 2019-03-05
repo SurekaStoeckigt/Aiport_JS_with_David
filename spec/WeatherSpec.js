f81332a9306bc5ae4f7c99bbc04d0a63e5a840fb
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-03-05 16:15:45 +0000

    passing tests for weather conditions

--- spec/WeatherSpec.js
+++ spec/WeatherSpec.js
@@ -2,11 +2,24 @@ describe('Weather', function() {
   var weather
   var condition
 
-  condition = 99
 
-  weather = new Weather(condition);
 
   it('is stormy when condition is 99', function() {
+    condition = 99
+    weather = new Weather(condition);
     expect(weather.storm()).toBe(true)
   });
+
+  it('is stormy when condition is 1', function() {
+    condition = 1
+    weather = new Weather(condition);
+    expect(weather.storm()).toBe(false)
+  });
+
+  it('is stormy when condition is 10', function() {
+    condition = 10
+    weather = new Weather(condition);
+    expect(weather.storm()).toBe(false)
+  });
+
 });
