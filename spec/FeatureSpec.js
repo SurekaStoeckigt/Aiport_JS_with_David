8569ebc7ce6dd811f6dca0ce1cbda0f5e5023051
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-03-07 14:18:25 +0000

    passing two feature tests

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -14,4 +14,11 @@ describe('', function() {
     expect(airport._landed).toContain(plane)
   });
 
+  it('planes can be instructed to takeoff and confirm that it has taken off', function(){
+    plane.land(airport)
+    expect(airport._landed).toContain(plane)
+    plane.takeoff()
+    expect(airport._landed).not.toContain(plane)
+  });
+
 });
--- src/Plane.js
+++ src/Plane.js
@@ -1,14 +1,19 @@
  function Plane(){
-   
+
  };
   // this.flying = true
   // this.weather = weather
 
 
 Plane.prototype.land = function(airport) {
-  // this.flying = false;
-  var this_airport = airport;
+  this_airport = airport;
   this_airport._landed.push(this)
+};
+
+Plane.prototype.takeoff = function() {
+ var plane_arr = this_airport._landed
+  plane_location = plane_arr.indexOf(this)
+  plane_arr.splice(plane_location, 1)
 };
 
 // Plane.prototype.takeoff = function(airport) {
